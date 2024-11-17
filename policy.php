b0325d35f9575f712006b768fee702b8d973809f
Phúc Phan <phucphan729@gmail.com>
2024-11-17 21:05:27 +0700

    file policy.php

---
+++ policy.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <?php require_once 'layout/header.php'?>
+    <link rel="stylesheet" href="../FE/css/policy.css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="header__top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-8">
+                        <div class="header__top__left">
+                            <ul>
+                                <li><i class="fa fa-envelope"></i>codenexus@gmail.com</li>
+                                <li>Giảm giá 10% phí thuê ấn phẩm khi đăng ký thẻ thành viên</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="header__top__right">
+                            <div class="header__top__right__social">
+                                <a href="#"><i class="fa fa-user"></i> Đăng ký</a>
+                            </div>
+                            <div class="header__top__right__auth">
+                                <a href="#"><i class="fa fa-user"></i> Đăng nhập</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="header__logo">
+                        <a href="./index.php"><img src="img/logo.png" alt=""></a>
+                    </div>
+                </div>
+        <div class="col-lg-7">
+                    <nav class="header__menu">
+                        <ul>
+                            <li class="active"><a href="./index.php">Trang chủ</a></li>
+                            <li><a href="./shop-grid.php">Sản phẩm</a></li>
+                            <li><a href="#">Giỏ hàng</a>
+                                <ul class="header__menu__dropdown">
+                                    <li><a href="./shop-details.html">Shop Details</a></li>
+                                    <li><a href="./shoping-cart.php">Shoping Cart</a></li>
+                                    <li><a href="./checkout.html">Check Out</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="./contact.html">Liên hệ</a></li>
+                            <li><a href="./policy.php">Chính sách</a></li>
+
+                        </ul>
+                    </nav>
+        </div>
+        </div>
+        <body>
+    <h3>Chính Sách Bán Hàng</h3>
+
+    <section>
+        <div class="policy-item">
+        <img src="../FE/img/policy/1.jpg" alt="Sản Phẩm Đảm Bảo Chất Lượng">
+        <h4>Sản Phẩm Đảm Bảo Chất Lượng</h4>
+        </div>
+        <ul>
+            <li>Chúng tôi cam kết 100% các mặt hàng được bán trên OGANI đều có bản quyền & được ký kết hợp đồng mua bán rõ ràng.</li>
+            <li>Chúng tôi cam kết 100% các sản phẩm đều có xuất xứ từ các nhà phát hành/ nhà cung cấp uy tín, chính hãng & được đảm bảo về chất lượng/ an toàn sức khoẻ.</li>
+        </ul>
+    </section>
+
+    <section>
+    <div class="policy-item">
+    <img src="../FE/img/policy/2.jpg" alt="Thông báo về việc thay đổi chi phí vận chuyển">
+    <h4>Thông Báo Về Việc Thay Đổi Chi Phí Vận Chuyển</h4>
+    </div>
+        
+        <ul>
+            <li>Phí vận chuyển mọi giá trị đơn hàng tại TP.Hồ Chí Minh (các quận nội thành, ngoại thành & các huyện): 19.000đ</li>
+            <li>Phí vận chuyển mọi giá trị đơn hàng có tại các tỉnh/ thành phố khác (ngoài TP.Hồ Chí Minh): 29.000đ</li>
+        </ul>
+    </section>
+
+    <section>
+    <div class="policy-item">
+    <img src="../FE/img/policy/3.jpg" alt="Khiếu Nại/Hoàn Tiền">
+    <h4>Khiếu Nại/Hoàn Tiền</h4>
+    </div>
+        
+        <ul>
+            <li>Trong trường hợp hàng bị hư hỏng trong quá trình vận chuyển/ giao hàng, chúng tôi cam kết sẽ giao lại hàng mới cho khách hàng.</li>
+            <li>Trong một số trường hợp vì một số lý do ngoài ý muốn (nguyên nhân đến từ OGANI) chúng tôi sẽ tiến hành thủ tục hoàn lại tiền cho bạn trong vòng 5 - 7 ngày làm việc kể từ ngày thông báo hoàn tiền.</li>
+        </ul>
+        <p style="color: #212529;">Mọi thắc mắc, khiếu nại, không hài lòng của bạn trong quá trình sử dụng sản phẩm, dịch vụ tại OGANI, bạn vui lòng liên hệ ngay với chúng tôi thông qua các kênh liên lạc sau:</p>
+        <ul>
+            <li>Hotline: 1900 6656 (bộ phận chăm sóc khách hàng)</li>
+            <li>Email: hotro@codenexus.com</a></li>
+        </ul>
+    </section>
+
+    <section>
+    <div class="policy-item">
+    <img src="../FE/img/policy/4.jpg" alt="Thời Gian Làm Việc">
+    <h4>Thời Gian Làm Việc</h4>
+    </div>
+        
+        <ul>
+            <li>Thứ 2 - thứ 6: từ 8h - 17h</li>
+            <li>Thứ 7: từ 8h – 12h</li>
+        </ul>
+        <p style="color: #212529;">Chúng tôi cam kết sẽ liên hệ bạn để tư vấn & hỗ trợ mọi thắc mắc, khiếu nại trong thời gian sớm nhất.</p>
+    </section>
+    <h3>Chính Sách Bảo Mật</h3>
+
+    <section>
+        <h4>Việc Bảo Lưu Thông Tin Khách Hàng</h4>
+        <p>Để sử dụng và trải nghiệm các dịch vụ của OGANI, bạn cần đăng ký tài khoản và cung cấp một số thông tin như: email, họ tên, số điện thoại, địa chỉ và một số thông tin khác. Bạn có thể tùy chọn không cung cấp cho chúng tôi một số thông tin nhất định nhưng sẽ có một chút bất tiện vì khi đó, bạn sẽ không thể được hưởng một số tiện ích mà những tính năng của chúng tôi cung cấp.</p>
+        <p>Mọi thông tin bạn nhập trên website sẽ được lưu trữ để sử dụng cho mục đích phản hồi yêu cầu của khách hàng, đưa ra những gợi ý phù hợp cho từng khách hàng khi mua sắm tại website, nâng cao chất lượng hàng hóa dịch vụ và liên lạc với khách hàng khi cần thiết.</p>
+
+        <h4>Mục Đích Sử Dụng Thông Tin</h4>
+        <ul>
+            <li>Gửi newsletter giới thiệu sản phẩm mới và những chương trình khuyến mãi của OGANI.</li>
+            <li>Cung cấp một số tiện ích, dịch vụ hỗ trợ khách hàng.</li>
+            <li>Nâng cao chất lượng dịch vụ khách hàng của OGANI.</li>
+            <li>Làm cơ sở giải quyết các vấn đề khiếu nại, tranh chấp phát sinh liên quan đến việc sử dụng sản phẩm, dịch vụ tại website OGANI.</li>
+            <li>Ngăn chặn những hoạt động vi phạm pháp luật Việt Nam.</li>
+        </ul>
+
+        <h4>Chia Sẻ Thông Tin</h4>
+        <p>Chúng tôi sẽ không chia sẻ thông tin của bạn trừ những trường hợp cụ thể sau đây:</p>
+        <ul>
+            <li>Chúng tôi chỉ đưa ra thông tin tài khoản và những thông tin cá nhân khác khi tin chắc rằng việc đưa những thông tin đó là phù hợp với luật pháp, bảo vệ quyền lợi, tài sản của người sử dụng dịch vụ, của OGANI và các bên thứ ba khác.</li>
+            <li>Theo yêu cầu pháp lý từ một cơ quan chính phủ hoặc khi chúng tôi tin rằng việc làm đó là cần thiết và phù hợp nhằm tuân theo các yêu cầu pháp lý.</li>
+            <li>Trong những trường hợp còn lại, chúng tôi sẽ có thông báo cụ thể cho bạn khi phải tiết lộ thông tin cho một bên thứ ba và thông tin này chỉ được cung cấp khi được sự phản hồi đồng ý từ phía bạn.</li>
+        </ul>
+
+        <h4>Chính Sách Cam Kết Bảo Mật Thông Tin Khách Hàng</h4>
+        <ul>
+            <li>Chúng tôi cam kết không tiết lộ thông tin khách hàng, không bán hoặc chia sẻ thông tin khách hàng cho bên thứ ba nào khác vì mục đích thương mại.</li>
+            <li>Chúng tôi cam kết mọi thông tin thanh toán giao dịch trực tuyến của khách hàng đều được bảo mật và an toàn.</li>
+            <li>Quý khách không nên trao đổi những thông tin cá nhân và thông tin thanh toán của mình cho bên thứ ba nào khác để tránh rò rỉ thông tin.</li>
+        </ul>
+
+        <p>OGANI hiểu rằng quyền lợi của bạn trong việc bảo vệ thông tin cá nhân cũng chính là trách nhiệm của chúng tôi nên trong bất kỳ trường hợp có thắc mắc, góp ý nào liên quan đến chính sách bảo mật của OGANI, vui lòng liên hệ với chúng tôi để được phúc đáp, giải quyết thắc mắc trong thời gian sớm nhất.</p>
+    </section>
+    <h3>Chính Sách Đổi Trả Hàng</h3>
+    <p>Để nâng cao chất lượng dịch vụ và trải nghiệm mua sắm của khách hàng, OGANI chúng tôi có những chính sách phù hợp khi khách hàng có nhu cầu đổi/ trả/ hoàn tiền sản phẩm. Chúng tôi luôn coi trọng và bảo vệ quyền lợi của người tiêu dùng với mong muốn mang đến cho quý khách chất lượng phục vụ tốt nhất.</p>
+    
+    
+    <h5>♥ Hướng dẫn các bước đổi/ trả hàng</h5>
+    <ul>
+        <li><strong>Bước 1:</strong> Vui lòng gửi email về địa chỉ hotro@codenexus.com để thông báo việc yêu cầu đổi/ trả hàng.</li>
+        
+        <li><strong>Bước 2:</strong> Nhân viên chăm sóc khách hàng sẽ liên hệ với bạn để xác nhận; kiểm tra & tiếp nhận hàng được yêu cầu đổi/ trả hàng (chỉ áp dụng đối với các trường hợp đổi/ trả hàng do lỗi xuất phát từ chúng tôi).</li>
+        
+        <p> (Trường hợp đổi hàng theo nhu cầu (màu sắc, kích thước...) bạn vui lòng liên hệ 1800 1100 để được hướng dẫn cụ thể)</p>
+        
+        <li><strong>Bước 3:</strong> Khi yêu cầu đổi trả được giải quyết, quý khách vui lòng gửi sản phẩm như hiện trạng khi nhận hàng ban đầu về địa chỉ văn phòng bao gồm sản phẩm và đầy đủ phụ kiện, giấy tờ chứng từ kèm theo (nếu có).</li>
+    </ul>
+
+</body>
+    <?php require_once 'layout/footer.php'?>
